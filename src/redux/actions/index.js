1ba8d5e1a27e79d7771b721f09ffaf24912cb398
aubreeabril <aubree.abril@gmail.com>
2018-11-20 17:56:46 -0500

    updating readme

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,10 +1,10 @@
-// import { GOOGLE_BOOKS_API_KEY, NY_TIMES_API_KEY } from "../../keys";
+import { GOOGLE_BOOKS_API_KEY, NY_TIMES_API_KEY } from "../../keys";
 const RAILS_API_URL = "https://book-club-api.herokuapp.com";
 const GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes?q=";
 const NYTIMES_API_URL =
   "https://api.nytimes.com/svc/books/v3/lists.json?api-key=";
-const GOOGLE_BOOKS_API_KEY = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
-const NY_TIMES_API_KEY = process.env.REACT_APP_NY_TIMES_API_KEY;
+// const GOOGLE_BOOKS_API_KEY = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+// const NY_TIMES_API_KEY = process.env.REACT_APP_NY_TIMES_API_KEY;
 
 export function getBooks(searchValue) {
   return dispatch => {
