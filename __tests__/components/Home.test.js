cb788d480654f9935fab3519cb2eb6373c6d338b
aubreeabril <aubree.abril@gmail.com>
2018-11-20 18:22:24 -0500

    tests not running yet

--- __tests__/components/Home.test.js
+++ __tests__/components/Home.test.js
@@ -5,6 +5,11 @@ import configureStore from "redux-mock-store"; // Smart components
 
 import Home from "../../src/components/Home.js";
 
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
 describe("<Home />", () => {
   describe("render()", () => {
     test("renders the component", () => {
